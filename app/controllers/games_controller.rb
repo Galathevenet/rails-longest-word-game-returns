d19677db9cff27fe9626226d422bc96333f2ab11
Gala Thevenet <galathevenet@hotmail.com>
2022-04-29 16:38:23 +0200

    score algo done. Problem to fix : even with letters from array, still not working

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,39 @@
 class GamesController < ApplicationController
+  require "json"
+  require "open-uri"
+
   def new
     @letters = ("A".."Z").to_a.sample(10)
   end
 
   def score
+    @letters = params[:letters].split
+    @word = params[:word]
+    @exists = english_word?(@word)
+    @included = included?(@word.split, @letters)
+
+    if @included == true
+      if @exists == true
+        @result = "Congratulations! #{@word.upcase} is a valid English word!"
+      else
+        @result = "Sorry but #{@word.upcase} does not seem to be a valid English word..."
+      end
+    else
+      @result = "sorry, but #{@word.upcase} can't be built out of #{@letters.join(", ")}"
+    end
   end
+
+  private
+
+  def english_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    url_serialized = URI.open(url).read
+    result = JSON.parse(url_serialized)
+    result["found"]
+  end
+
+  def included?(word, letters)
+    (word.uniq - letters.uniq).empty?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 get 'new', to: 'games#new'
-  get 'score', to: 'games#score'
+get 'score', to: 'games#score'
+post 'score', to: 'games#score'
 end
